50ebbcd6ebd4504d9e695fd3b01d1dd76148f8f9
A.Sikorski <sikorski.aps@gmail.com>
2020-11-16 22:34:51 +0100

    150 test instances and data loading tool

---
+++ tools.py
@@ -0,0 +1,15 @@
+import json
+import numpy as np
+
+
+def load_instances(filename):
+	dataset = None
+	with open(filename, 'r') as file:
+		dataset = json.load(file)
+		
+	for difficulty, instances in dataset.items():
+		for instance in instances:
+			instance['puzzle'] = np.array(instance['puzzle'])
+			instance['solution'] = np.array(instance['solution'])
+	
+	return dataset
