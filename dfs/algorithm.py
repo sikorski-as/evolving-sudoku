c200ccc4f83a169d71411f9a6a41286d038ab81a
A.Sikorski <sikorski.aps@gmail.com>
2020-12-14 21:31:25 +0100

    [WIP]Slow DFS solver tests

--- dfs/algorithm.py
+++ dfs/algorithm.py
@@ -1,7 +1,8 @@
 import time
 
-from evaluation import is_row_valid, is_column_valid, is_square_valid, _validate
+import tools
 from dfs import config
+from evaluation import is_row_valid, is_column_valid, is_square_valid, _validate
 
 
 def _precompute_possibilities(grid):
@@ -38,22 +39,6 @@ def _precompute_possibilities(grid):
     return possibilities
 
 
-def run(cfg: config.DFSConfig) -> None:
-    sudoku = cfg.sudoku_instance
-    possibilities = _precompute_possibilities(sudoku)
-
-    start = time.time()
-    solution = _dfs(sudoku, possibilities)
-    end = time.time()
-
-    print('solution found by DFS in {} seconds:'.format(end - start))
-    print(solution)
-    print('repetitions:', _validate(solution))
-    print('solution from dataset:')
-    print(cfg.sudoku_solution)
-    print('are they the same?', solution == cfg.sudoku_solution)
-
-
 def sudoku_iterator(current_row, current_column):
     nrow = current_row + 1
     ncol = current_column
@@ -73,7 +58,8 @@ def _dfs(sudoku, possibilities, row=0, col=0):
     else:
         for possible_number in possibilities[(row, col)]:
             sudoku[row, col] = possible_number
-            valid = is_row_valid(sudoku, row, col) and is_column_valid(sudoku, row, col) and is_square_valid(sudoku, row, col)
+            valid = is_row_valid(sudoku, row, col) and is_column_valid(sudoku, row, col) and is_square_valid(sudoku,
+                                                                                                             row, col)
             if valid:
                 nrow, ncol = sudoku_iterator(row, col)
                 solution = _dfs(sudoku, possibilities, nrow, ncol)
@@ -83,10 +69,60 @@ def _dfs(sudoku, possibilities, row=0, col=0):
                     continue
 
         sudoku[row, col] = 0
-        print('failed, backtracking ({}, {})'.format(row, col))
+        # print('failed, backtracking ({}, {})'.format(row, col))
         return None
 
 
+def test():
+    timings = []
+    repetitions = []
+    are_the_same = []
+
+    instances = tools.load_instances("../data/instances.json")
+    limit = 50
+    for i, instance in enumerate(instances['easy'][:limit], start=1):
+        puzzle, dataset_solution = instance['puzzle'], instance['solution']
+        possibilities = _precompute_possibilities(puzzle)
+
+        print(f'Starting instance #{i}')
+
+        start = time.time()
+        dfs_solution = _dfs(puzzle, possibilities)
+        end = time.time()
+
+        time_taken = end - start
+        timings.append(time_taken)
+
+        reps = _validate(dfs_solution)
+        repetitions.append(reps)
+
+        same = (dfs_solution == dataset_solution).all()
+        are_the_same.append(same)
+
+        print(f'Solution found by DFS after {time_taken} seconds, {reps} repetitions, are solutions the same: {same}')
+
+    print('     Timings:', timings)
+    print(' Repetitions:', repetitions)
+    print('Are the same:', are_the_same)
+
+
+def run(cfg: config.DFSConfig) -> None:
+    sudoku = cfg.sudoku_instance
+    possibilities = _precompute_possibilities(sudoku)
+
+    start = time.time()
+    solution = _dfs(sudoku, possibilities)
+    end = time.time()
+
+    print('solution found by DFS in {} seconds:'.format(end - start))
+    print(solution)
+    print('repetitions:', _validate(solution))
+    print('solution from dataset:')
+    print(cfg.sudoku_solution)
+    print('are they the same?', solution == cfg.sudoku_solution)
+
+
 if __name__ == '__main__':
-    cfg = config.DefaultConfig
-    run(cfg)
+    # cfg = config.DefaultConfig
+    # run(cfg)
+    test()
